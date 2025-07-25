af99219603f1766ce332671dbc76c705aa44a4ff
haraka-hub <kodakatabana@gmail.com>
2025-07-25 11:44:45 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,22 @@
+// firebase.js
+
+// Import Firebase libraries (via CDN in HTML)
+const firebaseConfig = {
+  apiKey: "AIzaSyAprsqWKtJvSIWKNhH9Y5fqrsJe7eOKNYQ",
+  authDomain: "haraka-connect-co-ug.firebaseapp.com",
+  databaseURL: "https://haraka-connect-co-ug-default-rtdb.firebaseio.com",
+  projectId: "haraka-connect-co-ug",
+  storageBucket: "haraka-connect-co-ug.firebasestorage.app",
+  messagingSenderId: "192137038338",
+  appId: "1:192137038338:web:eedeeea79843a28867351b",
+  measurementId: "G-LZPMG63L5D"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Export handles
+const auth = firebase.auth();
+const db = firebase.firestore();
+const storage = firebase.storage();
 
